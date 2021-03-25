27f55040f126bc8bb50c2b7d3c075fbded179391
akashdeepconnect <akashdeep.connect@gmail.com>
2021-03-25 07:03:41 -0400

    some changes

--- akash_pipeline/webservice_stage.py
+++ akash_pipeline/webservice_stage.py
@@ -3,5 +3,5 @@ from .akash_pipeline_stack import AkashPipelineStack
 class WebServiceStage(core.Stage):
     def __init__(self, scope: core.Construct, id:str, **kwargs):
         super().__init__(scope,id,**kwargs)
-        service=AkashPipelineStack(self,'')
+        service=AkashPipelineStack(self,'w')
         self.url_output =  service.url_output
